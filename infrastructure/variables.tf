1e9706982cea18b126d84bf334480b383b51e999
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2021-11-18 15:15:41 +0000

    Added database configuration in docker

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,17 +1,36 @@
-variable "product" {}
+variable "product" {
+  type = string
+}
 
-variable "component" {}
+variable "raw_product" {
+  type    = string
+  default = "rd" // jenkins-library overrides product for PRs and adds e.g. pr-123-ia
+}
+
+variable "component" {
+  type = string
+}
 
 variable "location" {
+  type    = string
   default = "UK South"
 }
 
-variable "env" {}
+variable "env" {
+  type = string
+}
 
-variable "subscription" {}
+variable "subscription" {
+  type = string
+}
 
-//variable "deployment_namespace" {}
+// variable "deployment_namespace" {}
 
 variable "common_tags" {
   type = map(any)
+}
+
+variable "postgresql_version" {
+  type    = string
+  default = "11"
 }
