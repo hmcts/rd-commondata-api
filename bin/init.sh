886cbb24a28c212087b313a54c6e78929ed9d91b
sahitya-desireddy <93707379+sahitya-desireddy@users.noreply.github.com>
2021-11-18 10:06:47 +0000

    Rdcc 3612 (#1)
    * Update Jenkinsfile_nightly
    
    * Update build.gradle
    
    * Update Jenkinsfile_CNP
    
    * Changed the config files according to the project specification
    
    * Push minor updates
    
    * Minor updates
    
    Co-authored-by: Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>

--- bin/init.sh
+++ bin/init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 #
 # Script to initialise project by executing steps as follows:
 #   - Replace port number
@@ -18,31 +18,25 @@ pushd $(dirname "$0")/.. > /dev/null
 
 slug="$product_name-$component_name"
 
-declare -a files_with_port=(.env Dockerfile README.md src/main/resources/application.yaml charts/rpe-spring-boot-template/values.yaml)
-declare -a files_with_slug=(build.gradle docker-compose.yml Dockerfile README.md ./infrastructure/main.tf ./src/main/java/uk/gov/hmcts/reform/demo/controllers/RootController.java charts/rpe-spring-boot-template/Chart.yaml)
+declare -a files_with_port=(.env Dockerfile README.md src/main/resources/application.yaml charts/rd-commondata-api/values.yaml)
+declare -a files_with_slug=(build.gradle docker-compose.yml Dockerfile README.md ./infrastructure/main.tf ./src/main/java/uk/gov/hmcts/reform/demo/controllers/RootController.java charts/rd-commondata-api/Chart.yaml)
 
 # Replace in CNP file
 for i in "Jenkinsfile_template"
 do
-  perl -i -pe "s/rpe/$product_name/g" ${i}
+  perl -i -pe "s/rd/$product_name/g" ${i}
   perl -i -pe "s/demo/$component_name/g" ${i}
 done
 
 # Replace image repo
-for i in "charts/rpe-spring-boot-template/values.yaml"
+for i in "charts/rd-commondata-api/values.yaml"
 do
   perl -i -pe "s/rpe/$product_name/g" ${i}
-  perl -i -pe "s/spring-boot-template/$component_name/g" ${i}
+  perl -i -pe "s/commondata-api.jar/$component_name/g" ${i}
 done
 
 #update maintainer name
-for i in "charts/rpe-spring-boot-template/Chart.yaml"
-do
-  perl -i -pe "s/rpe/$product_name/g" ${i}
-done
-
-#update app insights config
-for i in "src/main/resources/application.yaml"
+for i in "charts/rd-commondata-api/Chart.yaml"
 do
   perl -i -pe "s/rpe/$product_name/g" ${i}
 done
@@ -56,7 +50,7 @@ done
 # Replace spring-boot-template slug
 for i in ${files_with_slug[@]}
 do
-  perl -i -pe "s/spring-boot-template/$slug/g" ${i}
+  perl -i -pe "s/commondata-api.jar/$slug/g" ${i}
 done
 
 # Replace demo package in all files under ./src
@@ -64,7 +58,7 @@ find ./src -type f -print0 | xargs -0 perl -i -pe "s/reform.demo/reform.$package
 perl -i -pe "s/reform.demo/reform.$package/g" build.gradle
 
 # Rename charts directory
-git mv charts/rpe-spring-boot-template charts/${slug}
+git mv charts/rd-commondata-api charts/${slug}
 
 # Rename directory to provided package name
 git mv src/integrationTest/java/uk/gov/hmcts/reform/demo/ src/integrationTest/java/uk/gov/hmcts/reform/${package}
