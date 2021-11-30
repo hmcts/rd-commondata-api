0c026f8bb29ec5b702dfefc0e36992c821657544
sahitya-desireddy <93707379+sahitya-desireddy@users.noreply.github.com>
2021-11-30 14:25:02 +0000

    Rdcc 3612 (#9)
    * Added swagger config
    
    * Added swagger config
    
    * updated Readme
    
    Co-authored-by: Manish Paryani <85675203+ManishParyani@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/cdapi/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/cdapi/config/SwaggerConfiguration.java
@@ -2,12 +2,18 @@ package uk.gov.hmcts.reform.cdapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import uk.gov.hmcts.reform.cdapi.CommonDataApplication;
+
+import java.util.List;
+import java.util.Optional;
+
 
 @Configuration
 @EnableSwagger2
@@ -15,12 +21,25 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
+
         return new Docket(DocumentationType.SWAGGER_2)
             .useDefaultResponseMessages(false)
+            .genericModelSubstitutes(Optional.class)
+
             .select()
-            .apis(RequestHandlerSelectors.basePackage(CommonDataApplication.class.getPackage().getName() + ".controllers"))
+            .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
             .paths(PathSelectors.any())
-            .build();
+            .build()
+            .securitySchemes(apiKeyList());
+    }
+
+    private  List<SecurityScheme> apiKeyList() {
+        return
+            List.of(
+                new ApiKey("Authorization", "Authorization","header"),
+                new ApiKey("ServiceAuthorization", "ServiceAuthorization", "header")
+
+            );
     }
 
 }
