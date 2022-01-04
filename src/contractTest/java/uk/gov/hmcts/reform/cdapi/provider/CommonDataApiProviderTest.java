524e43bab0ff3c9995d2629f7350f0dae7af4fec
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2022-01-04 11:12:06 +0000

    Added Pact Test

--- src/contractTest/java/uk/gov/hmcts/reform/cdapi/provider/CommonDataApiProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/cdapi/provider/CommonDataApiProviderTest.java
@@ -11,17 +11,24 @@ import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.cdapi.controllers.CaseFlagApiController;
+import uk.gov.hmcts.reform.cdapi.controllers.CrdApiController;
 import uk.gov.hmcts.reform.cdapi.domain.CaseFlagDto;
+import uk.gov.hmcts.reform.cdapi.domain.CategoryKey;
+import uk.gov.hmcts.reform.cdapi.domain.HearingChannelDto;
 import uk.gov.hmcts.reform.cdapi.domain.ListOfValue;
 import uk.gov.hmcts.reform.cdapi.repository.CaseFlagRepository;
+import uk.gov.hmcts.reform.cdapi.repository.HearingChannelRepository;
 import uk.gov.hmcts.reform.cdapi.repository.ListOfVenueRepository;
 import uk.gov.hmcts.reform.cdapi.service.impl.CaseFlagServiceImpl;
+import uk.gov.hmcts.reform.cdapi.service.impl.CrdServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +43,8 @@ import static org.mockito.Mockito.when;
     host = "${PACT_BROKER_URL:localhost}",
     port = "${PACT_BROKER_PORT:80}", consumerVersionSelectors = {
     @VersionSelector(tag = "master")})
-@ContextConfiguration(classes = {CaseFlagApiController.class, CaseFlagServiceImpl.class})
+@ContextConfiguration(classes = {CaseFlagApiController.class, CrdApiController.class, CrdServiceImpl.class,
+    CaseFlagServiceImpl.class})
 @TestPropertySource(properties = {"loggingComponentName=CommonDataApiProviderTest"})
 @IgnoreNoPactsToVerify
 public class CommonDataApiProviderTest {
@@ -50,6 +58,11 @@ public class CommonDataApiProviderTest {
     @Autowired
     CaseFlagApiController caseFlagApiController;
 
+    @Autowired
+    CrdApiController crdApiController;
+
+    @MockBean
+    HearingChannelRepository hearingChannelRepository;
 
     @TestTemplate
     @ExtendWith(PactVerificationInvocationContextProvider.class)
@@ -63,11 +76,10 @@ public class CommonDataApiProviderTest {
     void before(PactVerificationContext context) {
         MockMvcTestTarget testTarget = new MockMvcTestTarget();
         testTarget.setControllers(
-            caseFlagApiController);
+            caseFlagApiController,crdApiController);
         if (nonNull(context)) {
             context.setTarget(testTarget);
         }
-
     }
 
     @State({"Case Flag Details Exist"})
@@ -103,5 +115,40 @@ public class CommonDataApiProviderTest {
         caseFlagDtos.add(caseFlagDto1);
         caseFlagDtos.add(caseFlagDto2);
         when(caseFlagRepository.findAll(anyString())).thenReturn(caseFlagDtos);
+    }
+
+    @State({"HearingChannels Details Exist"})
+    public void toReturnHearingChannelsByCategoryId() {
+        HearingChannelDto hearingChannelDto1 = new HearingChannelDto();
+        hearingChannelDto1.setParentKey("telephone");
+        hearingChannelDto1.setParentCategory("HearingChannel");
+        hearingChannelDto1.setLovOrder(1L);
+        hearingChannelDto1.setActive(true);
+        hearingChannelDto1.setHintTextCy(null);
+        hearingChannelDto1.setHintTextEn(null);
+        hearingChannelDto1.setValueCy(null);
+        hearingChannelDto1.setValueEn("Telephone - BTMeetme");
+        CategoryKey categoryKey = new CategoryKey();
+        categoryKey.setKey("telephone-btMeetMe");
+        hearingChannelDto1.setCategoryKey(categoryKey);
+
+        HearingChannelDto hearingChannelDto2 = new HearingChannelDto();
+        hearingChannelDto2.setParentKey("video");
+        hearingChannelDto2.setParentCategory("HearingChannel");
+        hearingChannelDto2.setLovOrder(2L);
+        hearingChannelDto2.setActive(true);
+        hearingChannelDto2.setHintTextCy(null);
+        hearingChannelDto2.setHintTextEn(null);
+        hearingChannelDto2.setValueCy(null);
+        hearingChannelDto2.setValueEn("Video - CVP");
+        CategoryKey categoryKey1 = new CategoryKey();
+        categoryKey1.setKey("telephone-CVP");
+        hearingChannelDto2.setCategoryKey(categoryKey1);
+
+        List<HearingChannelDto> hearingChannelDtos = new ArrayList<>();
+        hearingChannelDtos.add(hearingChannelDto1);
+        hearingChannelDtos.add(hearingChannelDto2);
+        when(hearingChannelRepository.findAll(ArgumentMatchers.<Specification<HearingChannelDto>>any()))
+            .thenReturn(hearingChannelDtos);
     }
 }
