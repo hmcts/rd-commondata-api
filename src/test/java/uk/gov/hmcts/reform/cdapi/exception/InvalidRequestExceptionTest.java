5d9184e1d7f6ebfbde69e0db924480f67f15268a
nayeemshaik-hmcts <nayeem.shaik@hmcts.net>
2022-02-09 16:25:38 +0000

    fixed sonar issues

--- src/test/java/uk/gov/hmcts/reform/cdapi/exception/InvalidRequestExceptionTest.java
+++ src/test/java/uk/gov/hmcts/reform/cdapi/exception/InvalidRequestExceptionTest.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class InvalidRequestExceptionTest {
+class InvalidRequestExceptionTest {
 
     @Test
-    public void test_handle_invalid_request_exception() {
+    void test_handle_invalid_request_exception() {
         InvalidRequestException invalidRequestException = new InvalidRequestException("Bad Request");
         assertNotNull(invalidRequestException);
         assertEquals(HttpStatus.BAD_REQUEST.getReasonPhrase(), invalidRequestException.getMessage());
