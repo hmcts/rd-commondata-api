0fce19b2927ce7d3747b82b8155fcfcfc25d511b
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2022-03-01 13:14:15 +0000

    RDCC-3800 updated test cases

--- src/functionalTest/java/uk/gov/hmcts/reform/cdapi/AuthorizationFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/cdapi/AuthorizationFunctionalTest.java
@@ -85,7 +85,7 @@ public abstract class AuthorizationFunctionalTest {
     @AfterAll
     public static void destroy() {
         log.info("Deleting user from sidam");
-        //emailsTobeDeleted.forEach(email -> idamOpenIdClient.deleteSidamUser(email));
+        emailsTobeDeleted.forEach(email -> idamOpenIdClient.deleteSidamUser(email));
     }
 
     public void validateErrorResponse(ErrorResponse errorResponse, String expectedErrorMessage,
