0244d32762357bc45f07b028300c1025668a8d06
shivajilokare <shivaji.lokare@HCTS.NET>
2022-03-16 13:48:17 +0000

    updated script

--- src/main/resources/db/migration/V1_3__drop_view_table_mv_list_of_values.sql
+++ src/main/resources/db/migration/V1_3__drop_view_table_mv_list_of_values.sql
@@ -1,2 +1,12 @@
 DROP TABLE hearing_channel;
+DROP TABLE hearing_type;
+DROP TABLE hearing_priority;
+DROP TABLE non_standard_duration_codes;
+DROP TABLE case_type;
+DROP TABLE cancellation_reasons;
+DROP TABLE interpreter_and_sign_language;
+DROP TABLE additional_facilities;
+DROP TABLE panel_member_type;
+DROP TABLE additional_facilities;
+DROP VIEW mv_list_of_values;
 commit;
