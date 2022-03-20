1ed5c97cac8119db9c1d60cb4d81e63a61efbf7d
ishratt <tunazzina.ishrat@hmcts.net>
2022-03-20 14:34:20 +0000

    RDCC-4236: cve fix and checkstyle issue fix

--- src/functionalTest/java/uk/gov/hmcts/reform/cdapi/CommonDataApiFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/cdapi/CommonDataApiFunctionalTest.java
@@ -20,7 +20,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SerenityTest
 @SpringBootTest
