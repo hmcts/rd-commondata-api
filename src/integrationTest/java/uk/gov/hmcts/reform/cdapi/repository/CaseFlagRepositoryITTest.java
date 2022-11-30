130068de28e55d12447ac55eb30d7dbc463d30db
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-11-30 14:28:03 +0000

    RDCC-5916:Changes made to enhance commondata repository test cases

--- src/integrationTest/java/uk/gov/hmcts/reform/cdapi/repository/CaseFlagRepositoryITTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/cdapi/repository/CaseFlagRepositoryITTest.java
@@ -1,36 +1,126 @@
 package uk.gov.hmcts.reform.cdapi.repository;
 
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.cdapi.CdAuthorizationEnabledIntegrationTest;
-import uk.gov.hmcts.reform.cdapi.CommonDataApplication;
-import uk.gov.hmcts.reform.cdapi.SpringBootIntegrationTest;
+import uk.gov.hmcts.reform.cdapi.domain.CaseFlagDto;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 @TestPropertySource(properties = {"spring.config.location=classpath:application-test.yml"})
-
+@ExtendWith(MockitoExtension.class)
+@SuppressWarnings("all")
 public class CaseFlagRepositoryITTest {
 
     @Autowired
     CaseFlagRepository caseFlagRepository;
 
+
+    @ParameterizedTest
+    @MethodSource("caseFlagTestArgsProvider")
+    void should_return_list_of_case_flags_when_service_code_is_provided(
+        String serviceId,
+        List<CaseFlagDto> caseFlagDtosExpected,
+        int size) {
+
+        assertThat(caseFlagRepository).isNotNull();
+
+        List<CaseFlagDto> caseFlagDetails = caseFlagRepository.findAll(serviceId);
+        assertNotNull(caseFlagDetails);
+
+        assertThat(size).isEqualTo(caseFlagDetails.size());
+
+        assertThat(caseFlagDtosExpected.get(0).getId()).isEqualTo(caseFlagDetails.get(0).getId());
+
+        assertThat(caseFlagDtosExpected.get(0).getId()).isEqualTo(caseFlagDetails.get(0).getId());
+        assertThat(caseFlagDtosExpected.get(0).getFlagCode()).isEqualTo(caseFlagDetails.get(0).getFlagCode());
+        assertThat(caseFlagDtosExpected.get(0).getValueCy()).isEqualTo(caseFlagDetails.get(0).getValueCy());
+        assertThat(caseFlagDtosExpected.get(0).getValueEn()).isEqualTo(caseFlagDetails.get(0).getValueEn());
+        assertThat(caseFlagDtosExpected.get(0).getCategoryId()).isEqualTo(caseFlagDetails.get(0).getCategoryId());
+
+        assertThat(caseFlagDtosExpected.get(0).getCategoryPath()).isEqualTo(caseFlagDetails.get(0).getCategoryPath());
+        assertThat(caseFlagDtosExpected.get(0).getHearingRelevant()).isEqualTo(caseFlagDetails.get(0).getHearingRelevant());
+        assertThat(caseFlagDtosExpected.get(0).getRequestReason()).isEqualTo(caseFlagDetails.get(0).getRequestReason());
+        assertThat(caseFlagDtosExpected.get(0).getIsParent()).isEqualTo(caseFlagDetails.get(0).getIsParent());
+
+
+    }
+
     @Test
-    void testFindAll() {
-        //doReturn(new ArrayList<>()).when(caseFlagRepository).findAll(anyString());
-        assertNotNull(caseFlagRepository);
+    void should_return_db_error_msg_when_service_code_is_null() {
+
+        String serviceId = null;
+
+        InvalidDataAccessResourceUsageException errorResponse =
+            Assertions.assertThrows(InvalidDataAccessResourceUsageException.class, () ->
+                caseFlagRepository.findAll(serviceId));
+
+        assertThat(errorResponse).isNotNull();
+        assertThat(errorResponse.getMessage().contains("could not extract ResultSet"));
+
+
+    }
+
+
+    private static Stream<Arguments> caseFlagTestArgsProvider() {
+
+        return Stream.of(
+            Arguments.of("XXXX", getCaseFlagDetails("XXXX"), 6),
+            Arguments.of("AAA1", getCaseFlagDetails("AAA1"), 9),
+            Arguments.of("", getCaseFlagDetails(""), 6),
+            Arguments.of("BBB", getCaseFlagDetails("BBB"), 6)
+        );
+    }
+
+    private static List<CaseFlagDto> getCaseFlagDetails(String serviceId) {
+
+        List<CaseFlagDto> caseFlagDtos = new ArrayList<>();
+        CaseFlagDto caseFlagDto;
+        if ("AAA1".equalsIgnoreCase(serviceId)) {
+            caseFlagDto = new CaseFlagDto();
+            caseFlagDto.setId(1);
+            caseFlagDto.setFlagCode("CATGRY");
+            caseFlagDto.setValueCy(StringUtils.EMPTY);
+            caseFlagDto.setValueEn("Case");
+            caseFlagDto.setCategoryId(0);
+            caseFlagDto.setCategoryPath(StringUtils.EMPTY);
+            caseFlagDto.setHearingRelevant(false);
+            caseFlagDto.setRequestReason(false);
+            caseFlagDto.setIsParent(true);
+            caseFlagDtos.add(caseFlagDto);
+        } else {
+            caseFlagDto = new CaseFlagDto();
+            caseFlagDto.setId(2);
+            caseFlagDto.setFlagCode("CATGRY");
+            caseFlagDto.setValueCy(StringUtils.EMPTY);
+            caseFlagDto.setValueEn("Party");
+            caseFlagDto.setCategoryId(0);
+            caseFlagDto.setCategoryPath(StringUtils.EMPTY);
+            caseFlagDto.setHearingRelevant(false);
+            caseFlagDto.setRequestReason(false);
+            caseFlagDto.setIsParent(true);
+
+            caseFlagDtos.add(caseFlagDto);
+        }
+
+        return caseFlagDtos;
     }
 }
--- src/integrationTest/java/uk/gov/hmcts/reform/cdapi/repository/ListOfVenueRepositoryITTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/cdapi/repository/ListOfVenueRepositoryITTest.java
@@ -1,29 +1,113 @@
 package uk.gov.hmcts.reform.cdapi.repository;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.cdapi.domain.ListOfValue;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.doReturn;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DataJpaTest
 @TestPropertySource(properties = {"spring.config.location=classpath:application-test.yml"})
+@ExtendWith(MockitoExtension.class)
+@SuppressWarnings("all")
 public class ListOfVenueRepositoryITTest {
 
 
     @Autowired
     ListOfVenueRepository listOfVenueRepository;
 
+
+    @ParameterizedTest
+    @MethodSource("listOfValuesTestArgsProvider")
+    void should_return_list_of_values_when_category_key_is_provided(
+        String categoryKey,
+        List<ListOfValue> listOfValuesExpected,
+        int size) {
+
+        assertThat(listOfVenueRepository).isNotNull();
+
+        List<ListOfValue> listOfValues = listOfVenueRepository.findListOfValues(categoryKey);
+
+        assertThat(listOfValues).isNotNull();
+        assertThat(size).isEqualTo(listOfValues.size());
+
+        if (size > 0) {
+
+            assertThat(listOfValuesExpected.get(0).getId()).isEqualTo(listOfValues.get(0).getId());
+            assertThat(listOfValuesExpected.get(0).getKey()).isEqualTo(listOfValues.get(0).getKey());
+            assertThat(listOfValuesExpected.get(0).getValue()).isEqualTo(listOfValues.get(0).getValue());
+        }
+    }
+
     @Test
-    void testFindListOfValues() {
-        //doReturn(new ArrayList<>()).when(listOfVenueRepository).findAll();
-        //assertNotNull(listOfVenueRepository.findAll());
-        assertNotNull(listOfVenueRepository);
+    void should_return_db_error_msg_when_category_key_is_null() {
+
+        String categoryKey = null;
+
+        InvalidDataAccessResourceUsageException errorResponse =
+            Assertions.assertThrows(InvalidDataAccessResourceUsageException.class, () ->
+                listOfVenueRepository.findListOfValues(categoryKey));
+
+        assertThat(errorResponse).isNotNull();
+        assertThat("could not extract ResultSet".contains(errorResponse.getMessage()));
+
+
+    }
+
+    private static Stream<Arguments> listOfValuesTestArgsProvider() {
+
+        String listingStatus = "ListingStatus";
+        String listingStatusSubChannel = "ListingStatusSubChannel";
+
+        String dummy = "dummy";
+        return Stream.of(
+            Arguments.of(listingStatus, getListOfValues(listingStatus), 1),
+            Arguments.of(listingStatusSubChannel, getListOfValues(listingStatusSubChannel), 1),
+            Arguments.of("", getListOfValues(""), 0),
+            Arguments.of(dummy, getListOfValues(dummy), 0)
+        );
+    }
+
+    private static List<ListOfValue> getListOfValues(String categoryKey) {
+
+        List<ListOfValue> listOfValues = new ArrayList<>();
+
+        ListOfValue listOfValue;
+
+        if ("ListingStatus".equalsIgnoreCase(categoryKey)) {
+
+            listOfValue = new ListOfValue();
+            listOfValue.setId("(0,6)");
+            listOfValue.setKey("test");
+            listOfValue.setValue("test");
+
+            listOfValues.add(listOfValue);
+        } else if ("ListingStatusSubChannel".equalsIgnoreCase(categoryKey)) {
+
+            listOfValue = new ListOfValue();
+            listOfValue.setId("(0,8)");
+            listOfValue.setKey("test");
+            listOfValue.setValue("test");
+
+            listOfValues.add(listOfValue);
+        }
+
+        return listOfValues;
     }
 }
