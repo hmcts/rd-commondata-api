5fef0e7a9f9205653c1d9e463c2531962fafc9be
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-12-05 07:20:28 +0000

    RDCC-eLinks : Changes made to add feign details

--- src/main/java/uk/gov/hmcts/reform/cdapi/elinks/ElinksConstants.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.cdapi.elinks;
-
-public class ElinksConstants {
-    private ElinksConstants() {
-    }
-
-
-    public static final String RD_ELINKS_API_PROXY = "rd_elinks_api_proxy";
-}
