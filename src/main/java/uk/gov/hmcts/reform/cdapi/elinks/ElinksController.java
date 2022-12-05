795a7da4c93ecbbd7a1d47d557bea8e88a17d85f
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-12-05 10:13:38 +0000

    RDCC-eLinks : Changes made to fix check style issues

--- src/main/java/uk/gov/hmcts/reform/cdapi/elinks/ElinksController.java
+++ src/main/java/uk/gov/hmcts/reform/cdapi/elinks/ElinksController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.http.ResponseEntity.ok;
-
 @RestController
 @Slf4j
 @RequestMapping(
