2ec6b23076c45253c06d0a0a625e6ea827c7a649
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-12-07 16:36:20 +0000

    RDCC-eLinks : Changes made to add logs

--- src/main/java/uk/gov/hmcts/reform/cdapi/elinks/ElinksServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/cdapi/elinks/ElinksServiceImpl.java
@@ -1,12 +1,17 @@
 package uk.gov.hmcts.reform.cdapi.elinks;
 
+import feign.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.cdapi.elinks.feign.ElinksFeignClient;
 
+import static java.util.Objects.nonNull;
+
 @Service
+@Slf4j
 public class ElinksServiceImpl implements ElinkService {
 
 
@@ -25,10 +30,22 @@ public class ElinksServiceImpl implements ElinkService {
 
     @Override
     public ResponseEntity<Object> retrieveBaseLocation() {
-
+        log.info("ElinksServiceImpl.retrieveBaseLocation  Start ============");
+        Response response = null;
+        try {
+            response = elinksFeignClient.getBaseLocationDetails();
+            log.info("ElinksServiceImpl.elinksFeignClient Response == " + response.body());
+        } catch (Exception exp ){
+            log.error(exp.getMessage());
+        }finally {
+        if (nonNull(response)) {
+            response.close();
+        }
+    }
         return ResponseEntity
             .status(HttpStatus.OK)
-            .body(elinksFeignClient.getBaseLocationDetails());
+            .body(response);
+
     }
 
 
