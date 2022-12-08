50640fb4cdcb87eeab77a1b31dc6c275f5603f37
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-12-08 10:47:08 +0000

    RDCC-5936:Changes made to add feign client logger

--- src/main/java/uk/gov/hmcts/reform/cdapi/elinks/configuration/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/cdapi/elinks/configuration/FeignConfiguration.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.reform.cdapi.elinks.configuration;
 
+import feign.Logger;
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
+import feign.slf4j.Slf4jLogger;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
@@ -9,6 +11,7 @@ import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import uk.gov.hmcts.reform.cdapi.elinks.feign.ElinksFeignClient;
 
 @Configuration
 @Slf4j
@@ -21,4 +24,15 @@ public class FeignConfiguration {
     ) {
         return new SpringFormEncoder(new SpringEncoder(messageConverters));
     }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public Logger logger() {
+        return new Slf4jLogger(ElinksFeignClient.class);
+    }
+
 }
