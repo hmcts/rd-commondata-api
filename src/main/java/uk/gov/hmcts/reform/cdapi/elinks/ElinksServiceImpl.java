1c538cc7206d43f2fd972e2a24a8b6569c9b8b59
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-12-08 08:04:05 +0000

    RDCC-eLinks : Changes made to add logs

--- src/main/java/uk/gov/hmcts/reform/cdapi/elinks/ElinksServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/cdapi/elinks/ElinksServiceImpl.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.cdapi.elinks;
 import feign.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.cdapi.elinks.feign.ElinksFeignClient;
