c162297d917957a59443091e6e8471ab1ab15855
m_659064 <vilas.shelke@HMCTS.NET>
2023-01-09 11:30:42 +0000

    RDCC-6037: 1. Need to show only those parent flags those are having available_externally = true.  2. When flag is parent and it don't have any child flags need to exclude such a flags from response.

--- src/contractTest/java/uk/gov/hmcts/reform/cdapi/provider/CommonDataApiProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/cdapi/provider/CommonDataApiProviderTest.java
@@ -122,7 +122,7 @@ public class CommonDataApiProviderTest {
         List<CaseFlagDto> caseFlagDtos = new ArrayList<>();
         caseFlagDtos.add(caseFlagDto1);
         caseFlagDtos.add(caseFlagDto2);
-        when(caseFlagRepository.findAll(anyString(), anyBoolean())).thenReturn(caseFlagDtos);
+        when(caseFlagRepository.findAll(anyString())).thenReturn(caseFlagDtos);
     }
 
     @State({"ListOfCategories Details Exist"})
