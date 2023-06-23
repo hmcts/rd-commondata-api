599a5fc35b2ae9e4bf5d0d74b110ed562846b68f
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-06-23 06:43:00 +0100

    Fix feign context issue

--- src/main/java/uk/gov/hmcts/reform/cdapi/CommonDataApplication.java
+++ src/main/java/uk/gov/hmcts/reform/cdapi/CommonDataApplication.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.reform.cdapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
@@ -14,6 +16,7 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
 @EnableJpaRepositories
 @SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.cdapi", "uk.gov.hmcts.reform.idam"})
 @EnableCaching
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 @EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.cdapi" },
     basePackageClasses = {IdamApi.class, ServiceAuthorisationApi.class})
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
