a735a18157e5e29972e54e4cea45f8170d090945
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-06-23 05:16:47 +0100

    Scan base packages

--- src/main/java/uk/gov/hmcts/reform/cdapi/CommonDataApplication.java
+++ src/main/java/uk/gov/hmcts/reform/cdapi/CommonDataApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
@@ -12,7 +13,7 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @EnableJpaAuditing
 @EnableJpaRepositories
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.cdapi", "uk.gov.hmcts.reform.idam"})
 @EnableCaching
 @EnableFeignClients(basePackageClasses = {IdamApi.class, ServiceAuthorisationApi.class})
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
