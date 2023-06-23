4c437e75b958e65dd22fd29aa697208c23b49e0f
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-06-23 05:01:11 +0100

    Fix class loading issues

--- src/main/java/uk/gov/hmcts/reform/cdapi/CommonDataApplication.java
+++ src/main/java/uk/gov/hmcts/reform/cdapi/CommonDataApplication.java
@@ -6,17 +6,15 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 
 @EnableJpaAuditing
 @EnableJpaRepositories
-@SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.cdapi", "uk.gov.hmcts.reform.idam"})
+@SpringBootApplication
 @EnableCaching
-@EnableFeignClients(basePackages = {
-    "uk.gov.hmcts.reform.cdapi" },
-    basePackageClasses = { IdamApi.class }
-)
+@EnableFeignClients(basePackageClasses = {IdamApi.class, ServiceAuthorisationApi.class})
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class CommonDataApplication {
 
