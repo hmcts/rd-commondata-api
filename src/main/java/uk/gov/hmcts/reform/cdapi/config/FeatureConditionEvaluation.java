d33810b5fba41a404368302941ebe4c2fe368343
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-07-24 12:36:03 +0100

    DTSRD-59: address import order

--- src/main/java/uk/gov/hmcts/reform/cdapi/config/FeatureConditionEvaluation.java
+++ src/main/java/uk/gov/hmcts/reform/cdapi/config/FeatureConditionEvaluation.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.reform.cdapi.config;
 
-import java.util.Map;
-
 import com.auth0.jwt.JWT;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,6 +14,8 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import uk.gov.hmcts.reform.cdapi.exception.ForbiddenException;
 import uk.gov.hmcts.reform.cdapi.service.FeatureToggleService;
+
+import java.util.Map;
 
 import static java.util.Objects.nonNull;
 import static org.apache.commons.lang3.BooleanUtils.isNotTrue;
