cdc0b23f528264c5b9677738a98e02c91111e45c
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-09-06 09:29:29 +0100

    Fix jakarta.inject-api version issue

--- src/main/java/uk/gov/hmcts/reform/cdapi/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/cdapi/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.cdapi.config;
 
+import jakarta.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -27,7 +28,6 @@ import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
 import uk.gov.hmcts.reform.cdapi.oidc.JwtGrantedAuthoritiesConverter;
 
 import java.util.List;
-import jakarta.inject.Inject;
 
 @Configuration
 @ConfigurationProperties(prefix = "security")
