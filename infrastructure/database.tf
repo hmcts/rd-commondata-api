621e9620768fd8dbfa6b8dfe5debd6a54b8ad0c8
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-09-19 19:59:12 +0100

    DTSRD-1234.provision terraform v14 server

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -25,18 +25,21 @@ module "postgresql" {
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   name          = join("-", [var.component, "POSTGRES-USER"])
   value         = "dbcommondata@rd-commondata-api-postgres-db-v11-demo"
+  //value         = module.db-common-data-v11.user_name
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name          = join("-", [var.component, "POSTGRES-PASS"])
   value         = "!mbj?Q[wDX9w=@ua"
+ // value         = module.db-common-data-v11.postgresql_password
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
   name          = join("-", [var.component, "POSTGRES-HOST"])
   value         = "rd-commondata-api-postgres-db-v11-demo.postgres.database.azure.com"
+ /// value         = module.db-common-data-v11.host_name
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
@@ -62,6 +65,7 @@ data "azurerm_key_vault" "s2s_key_vault" {
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   name          = join("-", [var.component, "POSTGRES-DATABASE"])
   value         = "dbcommondata"
+ // value         = module.db-common-data-v11.postgresql_database
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
