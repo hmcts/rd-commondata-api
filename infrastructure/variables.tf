e8b1935d103125eb0ef40f802bfee53adb6c37c6
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-09-19 22:03:37 +0100

    DTSRD-1176

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -26,7 +26,6 @@ variable "common_tags" {
   type = map(any)
 }
 variable "aks_subscription_id" {
-  default = "d025fece-ce99-4df2-b7a9-b649d3ff2060"
 }
 variable "team_contact" {
   type        = string
