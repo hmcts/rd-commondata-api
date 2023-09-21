6b492564d777c710d446fa41c532f5567ea728aa
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-09-21 15:55:42 +0100

    DTSRD-1176

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,3 +16,82 @@ resource "azurerm_resource_group" "rd-commondata-api-postgres-db-v14-demo" {
   location = var.location
 }
 
+# Create the database server
+# Name and resource group name will be defaults (<product>-<component>-<env> and <product>-<component>-data-<env> respectively)
+module "postgresql" {
+  source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  admin_user_object_id = var.jenkins_AAD_objectId
+  business_area        = "cft"
+  common_tags          = var.common_tags
+  component            = var.component
+  env                  = var.env
+  pgsql_databases = [
+    {
+      name = "rd-commondata-api"
+    }
+  ]
+  pgsql_version        = "14"
+  product              = var.product
+  name               = join("-", [var.product, var.component, "postgres-db", "v14"])
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name          = join("-", [var.component, "POSTGRES-USER"])
+  value         = "dbcommondata"
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name          = join("-", [var.component, "POSTGRES-PASS"])
+  value         = "dbcommondata"
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name          = join("-", [var.component, "POSTGRES-HOST"])
+  value         = module.postgresql.fqdn
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  name          = "microservicekey-rd-commondata-api"
+  key_vault_id  = data.azurerm_key_vault.s2s_key_vault.id
+}
+
+
+resource "azurerm_key_vault_secret" "common_data_s2s_secret" {
+  name          = "common-data-api-s2s-secret"
+  value         = data.azurerm_key_vault_secret.s2s_secret.value
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+}
+
+data "azurerm_key_vault" "s2s_key_vault" {
+  name                = local.s2s_key_vault_name
+  resource_group_name = local.s2s_vault_resource_group
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name          = join("-", [var.component, "POSTGRES-DATABASE"])
+  value         = "dbcommondata"
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name          = join("-", [var.component, "POSTGRES-PORT"])
+  value         = "5432"
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+}
+
+data "azurerm_key_vault" "rd_key_vault" {
+  name                = local.key_vault_name
+  resource_group_name = local.key_vault_name
+}
+
+
+
+
