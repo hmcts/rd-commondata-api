93faa5aef6b39a753ed1d14e01a7d2506ed54e07
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-10-04 13:34:05 +0100

    DTSRD-1213.reverting the changes done for upgrade to postgresV15

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,4 @@
 locals {
-  tags = (merge(
-    var.common_tags,
-    tomap({
-      "Team Contact" = var.team_contact
-      "Destroy Me"   = var.destroy_me
-    })
-  ))
   key_vault_name          = join("-", [var.product, var.env])
   s2s_key_vault_name        = join("-", ["s2s", var.env])
   s2s_vault_resource_group  = join("-", ["rpe-service-auth-provider", var.env])
