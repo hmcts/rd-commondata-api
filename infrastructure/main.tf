1f51ca29b05c98b7d96b556f80524e2639d1b23e
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-10-10 14:41:21 +0100

    DTSRD-1234.Changing Database name for V15

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -100,12 +100,12 @@ module "db-common-data-v15" {
   pgsql_version   = "15"
   product         = var.product-V15
   name            = join("-", [var.product-V15, var.component-V15])
-  username        = join("@", [var.database_name, join("-", [var.product-V15, var.component-V15])])
+  //username        = join("@", [var.database_name, join("-", [var.product-V15, var.component-V15])])
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER-V15" {
   name          = join("-", [var.component, "POSTGRES-USER-V15"])
-  value         = module.db-common-data-v15.username
+  value         = var.username
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
