821e7a77c1ed5bc9b7630150cc7b3d1f908ddfa4
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-03-22 11:47:49 +0000

    Fixing query to fetch listof values category by service id acc to changed req

--- src/integrationTest/resources/db/testmigration/V1_6__Alter_List_Of_Values.sql
+++ src/integrationTest/resources/db/testmigration/V1_6__Alter_List_Of_Values.sql
@@ -7,6 +7,7 @@ ADD CONSTRAINT compKey UNIQUE (categorykey,key,serviceid);
 
 INSERT INTO List_Of_Values (CategoryKey,ServiceID,Key,Value_EN,Value_CY,HintText_EN,HintText_CY,Lov_Order,ParentCategory,ParentKey,Active)
 VALUES ('HearingChannel','BBA3','telephone','Telephone',null,null,null,2,null,null,'Y'),
+('HearingChannel','BBA3','video','Video',null,null,null,3,null,null,'Y'),
 ('HearingChannel','BBA3','faceToFace','Face To Face',null,null,null,1,null,null,'Y'),
 ('HearingChannel','BBA3','notAttending','Not Attending',null,null,null,4,null,null,'Y'),
 ('HearingSubChannel','BBA3','telephone-btMeetMe','Telephone - BTMeetme',null,null,null,null,'HearingChannel','telephone','Y'),
