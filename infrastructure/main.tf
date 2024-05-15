3eddff953bf2254be0e41733474420d57cee7545
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-05-15 17:49:59 +0100

    Decomissioning single server

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,20 +4,6 @@ locals {
   s2s_vault_resource_group  = join("-", ["rpe-service-auth-provider", var.env])
 }
 
-module "db-common-data-v11" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product            = var.product
-  component          = var.component
-  name               = join("-", [var.product, var.component, "postgres-db", "v11"])
-  location           = var.location
-  subscription       = var.subscription
-  env                = var.env
-  postgresql_user    = "dbcommondata"
-  database_name      = "dbcommondata"
-  common_tags        = var.common_tags
-  postgresql_version = "11"
-}
-
 data "azurerm_key_vault" "rd_key_vault" {
   name                = local.key_vault_name
   resource_group_name = local.key_vault_name
@@ -36,36 +22,6 @@ data "azurerm_key_vault_secret" "s2s_secret" {
 resource "azurerm_key_vault_secret" "common_data_s2s_secret" {
   name          = "common-data-api-s2s-secret"
   value         = data.azurerm_key_vault_secret.s2s_secret.value
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name          = join("-", [var.component, "POSTGRES-USER"])
-  value         = module.db-common-data-v11.user_name
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  name          = join("-", [var.component, "POSTGRES-PASS"])
-  value         = module.db-common-data-v11.postgresql_password
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name          = join("-", [var.component, "POSTGRES-HOST"])
-  value         = module.db-common-data-v11.host_name
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name          = join("-", [var.component, "POSTGRES-DATABASE"])
-  value         = module.db-common-data-v11.postgresql_database
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  name          = join("-", [var.component, "POSTGRES-PORT"])
-  value         = "5432"
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
@@ -107,20 +63,20 @@ module "db-common-data-v16" {
 
 }
 
-resource "azurerm_key_vault_secret" "POSTGRES-USER-v16" {
-  name          = join("-", [var.component, "POSTGRES-USER-v16"])
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name          = join("-", [var.component, "POSTGRES-USER"])
   value         = module.db-common-data-v16.username
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
-resource "azurerm_key_vault_secret" "POSTGRES_HOST-v16" {
-  name          = join("-", [var.component, "POSTGRES-HOST-v16"])
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name          = join("-", [var.component, "POSTGRES-HOST"])
   value         = module.db-common-data-v16.fqdn
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
-resource "azurerm_key_vault_secret" "POSTGRES-PASS-v16" {
-  name          = join("-", [var.component, "POSTGRES-PASS-v16"])
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name          = join("-", [var.component, "POSTGRES-PASS"])
   value         = module.db-common-data-v16.password
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
@@ -131,8 +87,8 @@ resource "azurerm_key_vault_secret" "POSTGRES_DATABASE-v16" {
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
-resource "azurerm_key_vault_secret" "POSTGRES_PORT-v16" {
-  name          = join("-", [var.component, "POSTGRES-PORT-v16"])
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name          = join("-", [var.component, "POSTGRES-PORT"])
   value         = "5432"
   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
