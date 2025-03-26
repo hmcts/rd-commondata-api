71f7a7b5b2b71695c3649afb1296acf5ff77ca14
Antony Leons <antony@leons.dev>
2025-03-26 11:35:51 +0000

    remove concurrently

--- src/main/resources/db/migration/V1_16__add_indexes.sql
+++ src/main/resources/db/migration/V1_16__add_indexes.sql
@@ -1,4 +1,4 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_flag_details_category_id ON public.flag_details (category_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_flag_service_service_id_flag_code ON public.flag_service (service_id, flag_code);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_flag_details_id_category_id ON public.flag_details (id, category_id);
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_flag_service_upper_service_id ON public.flag_service (UPPER(service_id));
+CREATE INDEX IF NOT EXISTS idx_flag_details_category_id ON public.flag_details (category_id);
+CREATE INDEX IF NOT EXISTS idx_flag_service_service_id_flag_code ON public.flag_service (service_id, flag_code);
+CREATE INDEX IF NOT EXISTS idx_flag_details_id_category_id ON public.flag_details (id, category_id);
+CREATE INDEX IF NOT EXISTS idx_flag_service_upper_service_id ON public.flag_service (UPPER(service_id));
